fb0ac749e0094762f83ce85027d8f9a5c077d11d
Erik Tews <erik@datenzone.de>
2015-07-24 16:53:00 +0200

    Version number to 1.4.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup (
 	name = 'gensafeprime', 
-	version = '1.3', 
+	version = '1.4', 
 	description = 'Generate safe primes using OpenSSL (a prime p = 2*q+1, and q is prime too)', 
 	long_description = open('README.txt').read(),
 	author = 'Erik Tews',
